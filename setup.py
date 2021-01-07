49e968245ff8e1f5ab0a0a407c685fd45925286d
stdavis <stdavis@utah.gov>
2021-01-07 09:40:18 -0700

    chore(conductor): release v2.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='conductor',
-    version='2.0.4',
+    version='2.0.5',
     license='MIT',
     description='A bot to check on the status of a porter issue.',
     author='AGRC',
--- src/conductor/conductor.py
+++ src/conductor/conductor.py
@@ -243,7 +243,7 @@ def startup_local():
     """a way to access local secrets to run conductor locally
     """
     print('starting conductor...')
-    options = docopt(__doc__, version='conductor 2.0.4')
+    options = docopt(__doc__, version='conductor 2.0.5')
 
     def noop():
         return None
