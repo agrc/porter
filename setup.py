fbdea6e310e04e51697cc4501a092735e2bf8645
stdavis <stdavis@utah.gov>
2021-06-30 11:04:02 -0600

    fix: add missing dep

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'colorama==0.*',
         'docopt==0.*',
+        'flask==2.0.*',
         'psycopg2-binary==2.*',
         'PyGithub==1.*',
         'pygsheets==2.*',
