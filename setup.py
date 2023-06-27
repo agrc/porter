5e776680737e3866915785fa32ae2837cd0b6f19
steveoh <sgourley@utah.gov>
2023-06-27 11:53:17 -0600

    deps: update dependencies, switch to black, ruff, and pyproject

--- setup.py
+++ setup.py
@@ -10,62 +10,61 @@ from os.path import basename, splitext
 from setuptools import find_packages, setup
 
 setup(
-    name='conductor',
-    version='2.1.2',
-    license='MIT',
-    description='A bot to check on the status of porter issues.',
-    author='UGRC',
-    author_email='ugrc-developers@utah.gov',
-    url='https://github.com/agrc/porter',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    name="conductor",
+    version="2.1.2",
+    license="MIT",
+    description="A bot to check on the status of porter issues.",
+    author="UGRC",
+    author_email="ugrc-developers@utah.gov",
+    url="https://github.com/agrc/porter",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Issue Tracker': 'https://github.com/agrc/porter/issues',
+        "Issue Tracker": "https://github.com/agrc/porter/issues",
     },
-    keywords=['gis'],
+    keywords=["gis"],
     install_requires=[
-        'colorama==0.*',
-        'flask==2.*',
-        'psycopg2-binary==2.*',
-        'PyGithub==1.*',
-        'pygsheets==2.*',
-        'pyodbc==4.*',
-        'requests==2.*',
+        "colorama==0.*",
+        "flask==2.*",
+        "psycopg2-binary==2.*",
+        "PyGithub==1.*",
+        "pygsheets==2.*",
+        "pyodbc==4.*",
+        "requests==2.*",
     ],
     extras_require={
-        'cloud-run': [
-            'flask==2.*',
-            'gunicorn==20.*',
+        "cloud-run": [
+            "flask==2.*",
+            "gunicorn==20.*",
         ],
-        'tests': [
-            'pylint-quotes==0.2.*',
-            'pylint==2.*',
-            'pytest-cov==3.*',
-            'pytest-instafail==0.4.*',
-            'pytest-isort==3.*',
-            'pytest-mock==3.*',
-            'pytest-pylint==0.18.*',
-            'pytest-watch==4.*',
-            'pytest==7.*',
-            'yapf==0.*',
-        ]
+        "tests": [
+            "pytest-cov==4.*",
+            "pytest-instafail==0.5.*",
+            "pytest-isort==3.*",
+            "pytest-mock==3.*",
+            "pytest-ruff==0.*",
+            "pytest-watch==4.*",
+            "pytest==7.*",
+            "black==23.3.*",
+            "ruff==0.0.*",
+        ],
     },
     setup_requires=[
-        'pytest-runner',
+        "pytest-runner",
     ],
     entry_points={
-        'console_scripts': [
-            'conductor = conductor.conductor:startup',
-            'test_conductor = conductor.conductor:startup_local',
+        "console_scripts": [
+            "conductor = conductor.conductor:startup",
+            "test_conductor = conductor.conductor:startup_local",
         ]
     },
 )
