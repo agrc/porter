d7b57f57c3c0750de1362a49244d864603ac1766
steveoh <sgourley@utah.gov>
2025-06-30 17:22:41 -0600

    fix: update setup.py with agrc/python conventions

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@
 setup.py
 A module that installs conductor as a module
 """
-from glob import glob
-from os.path import basename, splitext
+
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
@@ -14,24 +14,26 @@ setup(
     version="2.5.11",
     license="MIT",
     description="A bot to check on the status of porter issues.",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     author="UGRC",
     author_email="ugrc-developers@utah.gov",
     url="https://github.com/agrc/porter",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
     ],
     project_urls={
         "Issue Tracker": "https://github.com/agrc/porter/issues",
     },
-    keywords=["gis"],
+    keywords=["gis", "issue-ops", "github", "automation", "workflow"],
     install_requires=[
         "colorama==0.*",
         "flask==3.*",
